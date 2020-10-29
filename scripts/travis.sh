e8f130ebe722dc507a5e93563a9c3f70f73c3263
Jason Mobarak <jason@swiftnav.com>
2020-10-29 05:58:06 -0700

    tweak

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,15 +3,30 @@
 set -ex
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    sudo apt-get install -y libyaml-dev
+
+  docker build --tag musl-builder -f Dockerfile.linux .
+  docker run -it --name musl-builder-run musl-builder
+
+  build_dir=build/x86_64-unknown-linux-musl/release/install
+  mkdir -p $build_dir
+
+  docker cp musl-builder-run:/home/rust/src/$build_dir/yamllint \
+    $build_dir/yamllint
+
+  docker rm musl-builder-run
+  docker rmi musl-builder
+
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+
+  cargo install pyoxidizer
+  pyoxidizer build --release
+
+  cp build/*/release/install/yamllint* .
+
+elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+
+  cargo install pyoxidizer
+  pyoxidizer build --release
+
+  cp build/*/release/install/yamllint* .
 fi
-
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew install libyaml
-fi
-
-cargo install --git https://github.com/indygreg/PyOxidizer.git --rev b3c1833a6185cd4379ab673c86975b44daa07b66 pyoxidizer
-
-pyoxidizer build --release
-
-cp build/*/release/install/yamllint* .
