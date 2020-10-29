658bbc11c674a1e71e9f1b5f85d2af71457f5c07
Jason Mobarak <jason@swiftnav.com>
2020-10-29 13:42:30 -0700

    clean-up

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -8,27 +8,29 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   docker run --name musl-builder-run musl-builder
 
   build_dir=build/x86_64-unknown-linux-musl/release/install
-  mkdir -p $build_dir
+  mkdir -p dist
 
   docker cp musl-builder-run:/home/rust/src/$build_dir/yamllint \
-    $build_dir/yamllint
+    dist/yamllint
 
   docker rm musl-builder-run
   docker rmi musl-builder
 
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 
-  export RUSTFLAGS="-C link-arg=-L/usr/local/opt/libyaml/lib -C link-arg=/usr/local/opt/libyaml/lib/libyaml.a"
+  export RUSTFLAGS="-C link-arg=/usr/local/opt/libyaml/lib/libyaml.a"
 
   cargo install pyoxidizer
   pyoxidizer build --release
 
-  cp build/*/release/install/yamllint* .
+  mkdir -p dist
+  cp build/*/release/install/yamllint dist/yamllint
 
 elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
 
   cargo install pyoxidizer
   pyoxidizer build --release
 
-  cp build/*/release/install/yamllint* .
+  mkdir -p dist
+  cp build/*/release/install/yamllint* dist
 fi
