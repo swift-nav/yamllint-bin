a111e47ce8898af2787864b52cde5f5c9e7ff232
Jason Mobarak <jason@swiftnav.com>
2020-10-28 23:46:17 -0700

    remove macos specialization

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,11 +3,6 @@
 set -ex
 
 cargo install pyoxidizer
-
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    cp pyoxidizer.bzl.macos pyoxidizer.bzl
-fi
-
 pyoxidizer build --release
 
 cp build/*/releaes/install/yamllint* .
