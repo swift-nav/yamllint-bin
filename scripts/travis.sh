89823c85ec0f46816238fcb58ad8c6606a6aa027
Jason Mobarak <jason@swiftnav.com>
2020-10-28 23:50:26 -0700

    libyaml

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,7 +2,15 @@
 
 set -ex
 
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    sudo apt install libyaml-dev
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew install libyaml
+fi
+
 cargo install pyoxidizer
 pyoxidizer build --release
 
-cp build/*/releaes/install/yamllint* .
+cp build/*/release/install/yamllint* .
