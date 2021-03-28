defe580b958c839595917b45f94e143fb40b3ba5
Jason Mobarak <jason@swiftnav.com>
2021-03-27 22:52:54 -0700

    chown

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -20,7 +20,7 @@ if [[ "$RUNNER_OS" == "Linux" ]]; then
            cargo run --release -- build \
              --target-triple x86_64-unknown-linux-musl --release --path yamllint"
 
-  sudo chown -R "$USER:$USER" .
+  sudo chown -R "$(id -u):$(id -g)" .
 
   build_dir=build/x86_64-unknown-linux-musl/release/install
   cp $build_dir/yamllint dist/yamllint-linux.bin
